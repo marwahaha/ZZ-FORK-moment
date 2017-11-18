8d3d8c0db0ef288ca6b2ad987557c97d73fcb991
Kunal Marwaha <marwahaha@berkeley.edu>
2017-11-18 14:51:02 -0500

    Km/test pr (#2)
    [misc] Description goes here

---
+++ typing-tests/test/moment/deprecate.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../../moment.d.ts" />
+import moment = require('../../../moment');
+import { module, test, expect } from '../qunit';
+import { deprecate } from '../../lib/utils/deprecate';
+
+module('deprecate');
+
+test('deprecate', function (assert) {
+    // NOTE: hooks inside deprecate.js and moment are different, so this is can
+    // not be test.expectedDeprecations(...)
+    var fn = function () {};
+    var deprecatedFn = deprecate('testing deprecation', fn);
+    deprecatedFn();
+
+    expect(0);
+});
